68da7cb438974a398e378323641c5a1df268a527
kiloVolt91 <pisciotta.enrico@gmail.com>
2023-03-09 17:52:36 +0100

    code refactoring - estrattore e-dis
    Ottimizzazione del codice di estrazione dei dati "e-dis" senza variazione delle funzioni principali.
    
    Aggiunta di controlli negli input di valori
    
    Aggiunta di controlli nelle manipolazioni dei dataframe

--- estrattori/edis/estrattore_dati_curve_edis.py
+++ estrattori/edis/estrattore_dati_curve_edis.py
@@ -1,222 +1,140 @@
 ## SCRIPT DI ESTRAZIONE AUTOMATICA DEI DATI DA FILE .CSV SCARICATO DAL PORTALE E-DISTRIBUZIONE.
 ## ASSICURARSI DI SCARICARE I FILE CON IL NOME CORRETTO ED INSERIRLI ALL'INTERNO DELLA CARTELLA:
-## "curve_edis/IT001E_POD_UNIVOCO/ANNO/TIPOLOGIA DI ENERGA"
-
-#INIZIALIZZAZIONE
-import pandas as pd
-import os
-import numpy as np
-import configparser
-from init import init_path
-
+## "curve_edis/IT001E_POD_UNIVOCO/ANNO/TIPOLOGIA DI ENERGIA"
 #N.B. verificare la correttezza di tutti i percorsi
 
-#apertura del file init.ini
-config = configparser.ConfigParser()
-config.read(init_path)
-origin_path = config['user']['origin_path_edis']
-pod_totali = config['user']['pod_totali']
-output_data_path = config['user']['output_data_path']
+#INIZIALIZZAZIONE
 
-#creazione del dizionario contenente i pod e la relativa descrizione - vedi file init.ini
-#N.B. inserire all'interno del dizionario tutti i POD disponibili e la relativa descrizione
-dizionario_pod = {}
-for i in range (1,int(pod_totali)+1):
-    dizionario_pod[config.get('POD', 'p'+str(i))] = config.get('utenza', 'u'+str(i))
+from init import * #Posizione del file init.ini 
+from funzioni import *
 
-#tolleranza di calcolo numerico - errore accettabile - verificare origine degli errori di calcolo numerico    
-toll = float(1e-6) 
-
-#creazione della lista contenente i nomi delle colonne del dataframe
-colonne = list(  config['dati_dataframe_edis']['colonne'].split(",")  )
-columns = np.array(colonne)
-
-#Inizializzazione dataframe dei dati enel a 15 minuti
-df_edis = pd.DataFrame(columns = columns) 
-os.chdir(origin_path)
-
-#Ciclo for per scansionare i nomi dei POD presenti nella cartella "curve e_dis". I nomi vengono inseriti all'interno del vettore "pod"
 #ELENCO DELLE CARTELLE POD DISPONIBILI
-pod = []   
+lista_pod = []   
 for name in os.listdir(origin_path):    
     if name[0:6] =='IT001E':
-        pod.append(name[0:18])
-print('POD disponibili: ', pod)
+        lista_pod.append(name[0:18])
+if lista_pod:
+    print('POD disponibili: ')
+    for pod in lista_pod:
+        print(lista_pod.index(pod)+1,']  ', pod, dizionario_pod[pod])
+else:
+    sys.exit('Nessun POD disponibile nella cartella indicata: '+ str(origin_path))
 
 #SELEZIONE DELLA CARTELLA POD 
-i_pod = int(input('Inserisci il numero di posizione del POD desiderato dalla lista: ')) # input numerico 1...n corrispondente con l'indice i-1 del vettore pod
-selezione = pod[i_pod-1]
-print('Hai selezionato il seguente POD: ', selezione, dizionario_pod[selezione])
-os.chdir(selezione)  #entro nella cartella del POD selezionato
+while True:
+    os.chdir(origin_path)
+    i_pod = input('Inserisci il numero di posizione del POD desiderato dalla lista: ') # input numerico 1...n corrispondente con l'indice i-1 del vettore pod
+    if i_pod.isnumeric() == False:
+        sys.exit('Il valore inserito non è un numero')
+    if int(i_pod) > len(lista_pod):
+        sys.exit('Il valore inserito eccede i POD disponibili')
+    selezione_pod = lista_pod[int(i_pod)-1]    
+    print('Hai selezionato il seguente POD: ', selezione_pod, dizionario_pod[selezione_pod])
+    os.chdir(selezione_pod)  #entro nella cartella del POD selezionato
+    break
 
-#SELEZIONE DELLA CARTELLA ANNO
-mesi_edis = ['gennaio', 'febbraio', 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre']  #Vettore dei nomi contenuti nei file di e-distribuzione
-giorni_mese = {'gennaio':31, 'febbraio':28, 'febbraio_bisestile':29, 'marzo':31, 'aprile':30, 'maggio':31, 'giugno':30, 'luglio':31, 'agosto':31, 'settembre':30, 'ottobre':31, 'novembre':30, 'dicembre':31} #dizionario contenente il numero di giorni per ciascun mese
-numero_mese = {'gennaio':'01', 'febbraio':'02', 'febbraio_bisestile':'02', 'marzo':'03', 'aprile':'04', 'maggio':'05', 'giugno':'06', 'luglio':'07', 'agosto':'08', 'settembre':'09', 'ottobre':'10', 'novembre':'11', 'dicembre':'12'} #dizionario di associazione mese:numero_del_mese
-anno = input("Inserisci l'anno di riferimento in formato AAAA: \n") #selezione anno 
+#SELEZIONE DELL'ANNO  
+lista_anni = []   
+for name in os.listdir(os.getcwd()):    
+    if (name[0:2] =='20') or (name[0:2] =='19'):
+        lista_anni.append(name[0:18])
+if lista_anni:
+    print('Anni disponibili: ')
+    for anno in lista_anni:
+        print(lista_anni.index(anno)+1,']  ', anno)
+else:
+    sys.exit('Nessun anno disponibile nella cartella indicata: '+ str(origin_path)) 
+while True:
+    i_anno = input("Seleziona l'anno di riferimento: \n") #selezione anno  
+    if i_anno.isnumeric() == False:
+        sys.exit('Il valore inserito non è un numero')
+    if int(i_anno) > len(lista_anni):
+        sys.exit('Il valore inserito eccede gli anni disponibili')
+    selezione_anno = lista_anni[int(i_anno)-1]    
+    print('Hai selezionato il seguente anno: ', selezione_anno)
+    os.chdir(selezione_anno)  #entro nella cartella del POD selezionato
+    break
 
-#Verifica di bisestilità dell'anno scelto, la verifica non è valida per gli anni bisestili secolari (divisibili per 100 e 400)
-#Aggiungere verifica per anni secolari
-if int(anno) % 4 == 0:
+#VERIFICA BISESTILITÀ
+if (int(selezione_anno)%4 == 0 and int(selezione_anno)%100 !=0) or (int(selezione_anno)%400 == 0):
     febbraio = 'febbraio_bisestile' #anno bisestile
 else:
-    febbraio = 'febbraio' #anno non bisestile
+    febbraio = 'febbraio' #anno non bisestile    
 mesi = ['gennaio', febbraio, 'marzo', 'aprile', 'maggio', 'giugno', 'luglio', 'agosto', 'settembre', 'ottobre', 'novembre', 'dicembre'] #vettore dei mesi con il mese di febbraio in dettaglio
-os.chdir(anno)
 
-#SELEZIONE DELLA CARTELLA ENERGETICA 
-#Aggiungere dei controlli
-tipo_energia = int(input("Tipologia di energia: inserisci 1 per l'energia attiva, inserisci 2 per l'energia reattiva induttiva, inserisci 3 per l'energia reattiva capacitiva: \n"))
-tipo_regime = int(input("Tipologia di regime di cessione: inserisci 1 per l'immissione di energia, 2 per il prelievo di energia: \n"))
-
-if (tipo_energia >=1) & (tipo_energia<=3):
-    if tipo_energia == 1:
-        tipo_energia = 'attiva'
-    if tipo_energia == 2:
-        tipo_energia = 'reattiva induttiva'
-    if tipo_energia == 3:
-        tipo_energia = 'reattiva capacitiva'
-else:
-    print('tipo energia: è stato inserito un valore errato')
-
-if (tipo_regime >=1) & (tipo_regime<=2):
-    if tipo_regime == 1:
-        tipo_regime = 'immessa'
-    if tipo_regime == 2:
-        tipo_regime = 'prelevata'
-else:
-    print('tipo regime: è stato inserito un valore errato')    
+#SELEZIONE DELLA TIPOLOGIA DI ENERGIA E DI REGIME
+while True:
+    tipo_energia = input("Tipologia di energia:\n 1) Energia Attiva\n 2) Energia Reattiva Induttiva\n 3) Energia Reattiva Capacitiva \n")
+    if tipo_energia.isnumeric() == False:
+        sys.exit('Il valore inserito non è un numero')
+    if int(tipo_energia) > 3:
+        sys.exit('Il valore inserito non è in lista')
+    while True:
+        if int(tipo_energia) == 1:
+            tipo_energia = 'attiva'
+            break
+        if int(tipo_energia) == 2:
+            tipo_energia = 'reattiva induttiva'
+            break
+        if int(tipo_energia) == 3:
+            tipo_energia = 'reattiva capacitiva'
+            break
+    print('Hai selezionato la seguente tipologia: ', tipo_energia)
+    break    
+while True:
+    tipo_regime = input("Tipologia di regime di cessione:\n 1) Immissione di Energia\n 2) Prelievo di Energia\n")
+    if tipo_regime.isnumeric() == False:
+        sys.exit('Il valore inserito non è un numero')
+    if int(tipo_regime) > 2:
+        sys.exit('Il valore inserito non è in lista')
+    while True:
+        if int(tipo_regime) == 1:
+            tipo_regime = 'immessa'
+            break
+        if int(tipo_regime) == 2:
+            tipo_regime = 'prelevata'
+            break
+    print('Hai selezionato il seguente regime: ', tipo_regime)
+    break
 os.chdir(tipo_energia+' '+tipo_regime)
 cwd = os.getcwd()
 
-
-#ESTRAZIONE DEI DATI E-DISTRIBUZIONE
-for m in range (0,12):    
-    if os.path.exists(cwd+'/ExportData_' + mesi_edis[m] + '.csv') == True:     
-        df_month = pd.read_csv('ExportData_' + mesi_edis[m] + '.csv', sep=';', index_col=False, decimal=",") #dati del file mensile di riferimento
-        df_edis = pd.concat([df_edis,df_month])
-    else: #nel caso in cui non sia stato trovato alcun file per il mese selezionato, lo spazio lasciato vuoto nel dataframe viene riempito di valori "NaN"
-        nan_vec = [np.nan]* giorni_mese[mesi[m]]  #vettore contenente n valori NaN tanti quanti i giorni del mese di riferimento
-        df_month = pd.DataFrame()
-
+#IMPORTAZIONE DEI DATI E-DISTRIBUZIONE IN UN DATAFRAME
+#I DATI SONO MENSILI CON INTERVALLO DI CAMPIONAMENTO PARI A 15 MINUTI
+df_edis_15m = pd.DataFrame(columns = columns) 
+for mese in mesi_edis:    
+    if os.path.exists(cwd+'/ExportData_' + mese + '.csv') == True:     
+        df_singolo_mese = pd.read_csv('ExportData_' + mese + '.csv', sep=';', index_col=False, decimal=",") #dati del file mensile di riferimento
+        df_edis_15m = pd.concat([df_edis_15m,df_singolo_mese])
+    else: 
+        #nel caso in cui non sia stato trovato alcun file per il mese selezionato, lo spazio lasciato vuoto nel dataframe viene riempito di valori "NaN"
+        nan_vec = [np.nan]* giorni_mese[mese]  #vettore contenente n valori NaN tanti quanti i giorni del mese di riferimento
+        df_singolo_mese = pd.DataFrame() # inizializzazione df
         #CREAZIONE DELLA COLONNA DEL DATAFRAME CONTENENTE LE DATE DA ASSOCIARE AI VALORI NAN
-        ind_mese = numero_mese[mesi[m]]
-        day_vec = []
-        for i in range(1,giorni_mese[mesi[m]]+1):
-            day_vec.append(str(i).zfill(2)+'/'+ind_mese+'/'+anno)          
-        df_month['Giorno'] = day_vec 
+        indice_mese = numero_mese[mese]
+        day_list = []
+        for i in range(1,giorni_mese[mese]+1):
+            day_list.append(str(i).zfill(2)+'/'+indice_mese+'/'+anno)          
+        df_singolo_mese['Giorno'] = day_list 
         #CREAZIONE DELLA COLONNA CONTENENTE I VALORI NAN
         for j in range (1, len(columns)):
-            df_month[columns[j]] = pd.DataFrame(nan_vec)
-        df_edis = pd.concat([df_edis,df_month])      
-        print('Dati assenti per il seguente mese: ' + mesi_edis[m])
+            df_singolo_mese[columns[j]] = pd.DataFrame(nan_vec)
+        df_edis_15m = pd.concat([df_edis_15m,df_singolo_mese])      
+        print('Dati assenti per il seguente mese: ' + mesi_edis[mesi_edis.index(mese)])
+df_edis_15m.reset_index(inplace=True) 
+df_edis_15m.drop(columns='index', inplace=True)
+
 os.chdir(origin_path)        
-        
-#MODIFICHE ALLA STRUTTURA DEL DATAFRAME
-df_edis.reset_index(inplace=True) 
-df_edis.drop(columns='index', inplace=True)
-nomi_colonne = list(df_edis.head(0))
 
-df_orario = df_edis['Giorno'].copy()
-headers_orari = ['Giorno \\ Ora']
+#CREAZIONE DI ALTRI DATAFRAME - DETTAGLIO ORARIO (UTILE PER GME) E DETTAGLIO MENSILE (UTILE PER DICHIARAZIONI E VERIFICHE)
+df_edis_1h = raggruppamento_orario_dataframe(df_edis_15m)
+verifica_integrita_df (df_edis_15m, df_edis_1h)
+df_edis_mese = creazione_riepilogo_mensile(df_edis_15m)
 
-#VERIFICA PRE-MANIPOLAZIONE DEL VALORE NUMERICO DEI DATI GREZZI
-check_iniziale = df_edis.iloc[:,1:97].astype(float).sum().sum() 
-print('ENERGIA PRE-MANIPOLAZIONE: ')
-print(check_iniziale)
+#ESPORTAZIONE RIEPILOGHI .CSV
+export_path = selezione_cartella_export_dati (output_data_path, selezione_pod, selezione_anno)
+esporta_csv(df_edis_15m, 'riepilogo_15min_', export_path, tipo_energia, tipo_regime)
+esporta_csv(df_edis_1h, 'riepilogo_orario_', export_path, tipo_energia, tipo_regime)
+esporta_csv(df_edis_mese, 'riepilogo_mensile_', export_path, tipo_energia, tipo_regime)
 
-#DATAFRAME ORARIO DELL'ENERGIA - CREAZIONE DI RAGGRUPPAMENTI PER CIASCUNA ORA DEL GIORNO DEI DATI A 15 MINUTI DI ENEL
-for k in range(0,24,1):
-    headers_orari.append(k+1)
-    df_sum = df_edis[nomi_colonne[4*k+1]].astype(float)+df_edis[nomi_colonne[4*k+2]].astype(float)+df_edis[nomi_colonne[4*k+3]].astype(float)+df_edis[nomi_colonne[4*k+4]].astype(float) #RAGGRUPPAMENTO PER MULTIPLI DI 4 SULLE COLONNE (4 QUARTI D'ORA)
-    df_orario=pd.concat([df_orario,df_sum], axis=1)
-df_orario.set_axis(headers_orari,axis=1, inplace=True)
-df_orario['Totale_Giorno'] = df_orario.iloc[:,1:25].sum(axis=1) #SOMMA DELL'ENENERGIA DI CIASCUNA ORA PER AVERE IL TOTALE GIORNALIERO
-Energia_annua = df_orario['Totale_Giorno'].sum() #SOMMA DELLE ENERGIE GIORNALIERE PER OTTENERE IL TOTALE ANNUO
-print('ENERGIA ANNO: ')
-print(Energia_annua)
-
-#INDIVIDUAZIONE DEL MESE DI RIFERIMENTO PER CIASCUN GIORNO - step necessario per l'estrazione del dataframe di riepilogo mensile
-dimensione = df_orario.shape[0]
-vec_m = []
-for i in range(0, dimensione, 1):
-    data = df_orario.iloc[i,0]
-    vec_m.append(data[3:5])          
-df_orario['mese'] = vec_m
-
-##CORREZIONE ORA LEGALE-ORA SOLARE - Creazione dell'ora n.25 che conterrà un solo valore
-df = df_orario.copy()
-df.drop(columns=['Totale_Giorno', 'mese'], inplace=True)
-empty = [np.nan]*df_orario.shape[0]
-df['25'] = empty
-drop=[]
-ora_switch = 3 #il cambio di orario avviene tra le 2:00 e le 3:00 nel caso di ora solare a marzo, l'ora viene portata indietro
-#in caso di ora legale, l'ora viene portata avanti
-
-for i in range(1, df.shape[0]):
-    a = str(df[headers_orari].iloc[i-1,0])
-    b = str(df[headers_orari].iloc[i,0])
-    if a == b:  #verifico l'esistenza di date identiche coincidenti con ora solare e legale        
-        mese = int(df[headers_orari].iloc[i-1,0][3:5])     
-        if mese == 3: #ORA SOLARE devo incollare il vettore i a partire dalla colonna 3 in posizione i-1 a partire dalla colonna 2
-            st = df.iloc[i,1:df.shape[1]].sum() + df.iloc[i-1,1:df.shape[1]].sum()        
-            for j in range (ora_switch, df.shape[1]-1):
-                df.iloc[i,j] = df.iloc[i-1,j]
-            df.iloc[i-1,24] = np.nan
-            drop.append(i-1)
-            sf = df.iloc[i,1:df.shape[1]].sum()
-
-#ORA LEGALE incollare il vettore i a partire dalla colonna 3 ed in posizione i-1 a partire dalla colonna 4
-            
-        elif mese == 10:
-            st = df.iloc[i,1:df.shape[1]].sum() + df.iloc[i-1,1:df.shape[1]].sum()      
-            for j in range (ora_switch, df.shape[1]-1):
-                df.iloc[i-1,j+1] = df.iloc[i,j]
-            drop.append(i)
-            sf = df.iloc[i-1,1:df.shape[1]].sum()
-        else:
-            print('Anomalia nei dati nel mese...')
-            print(mese)
-                
-if len(drop)>2:
-    print('VERIFICARE ANOMALIA DATE')
-                
-for i in range (0, len(drop)):
-    df = df.drop([drop[i]])
-
-# VERIFICA INTEGRITA' DEI DATI MANIPOLATI
-check_finale = df.iloc[:,1:df.shape[1]].sum().sum() 
-
-if check_finale - check_iniziale > toll:
-    print ("L'energia iniziale non corrisponde con quella finale")
-else:
-    print('Esito Positivo')
-
-#CREAZIONE E COLLOCAZIONE DEL FILE .CSV CONTENENTE I DATI MANIPOLATI ED ORDINATI
-data_path = os.path.join(output_data_path, 'dati_elaborati', selezione )
-if os.path.exists(data_path) != True: 
-    os.mkdir(data_path)
-
-os.chdir(data_path)
-data_path = os.path.join(data_path, 'e-dis')
-
-if os.path.exists(data_path) != True: 
-    os.mkdir(data_path)
-    
-os.chdir(data_path)
-data_path = os.path.join(data_path, anno)
-
-if os.path.exists(data_path) != True: 
-    os.mkdir(data_path)
-    
-export_path = os.path.join(data_path, 'riepilogo_15min_'+ tipo_energia + '_' + tipo_regime + '.csv')
-df_edis.to_csv(export_path, index=False, decimal =',', sep=';')
-export_path = os.path.join(data_path, 'riepilogo_orario_'+ tipo_energia + '_' + tipo_regime + '.csv')
-df.to_csv(export_path, index=False, decimal =',', sep=';')
-
-#CREAZIONE DATAFRAME MENSILE
-df_mensile = df_orario.groupby(['mese'])['Totale_Giorno'].sum()
-export_path = os.path.join(data_path, 'riepilogo_mensile_'+ tipo_energia + '_' + tipo_regime + '.csv')
-df_mensile.to_csv(export_path, index=False, decimal =',', sep=';')
-
-print ('Fine Operazione')
+print('Fine Operazioni')
---
+++ estrattori/edis/funzioni.py
@@ -0,0 +1,115 @@
+#FUNZIONI
+from init import*
+
+def raggruppamento_orario_dataframe(df):
+    nomi_colonne = list(df.columns)
+    df_orario = df['Giorno'].copy()
+    headers_orari = ['Giorno']
+    for k in range(0,24,1):
+        headers_orari.append('ora ' + str(k+1))
+        #RAGGRUPPAMENTO PER MULTIPLI DI 4 SULLE COLONNE (4 QUARTI D'ORA)
+        df_sum_iter = df[nomi_colonne[4*k+1]].astype(float)+df[nomi_colonne[4*k+2]].astype(float)+df[nomi_colonne[4*k+3]].astype(float)+df[nomi_colonne[4*k+4]].astype(float)
+        df_orario = pd.concat([df_orario,df_sum_iter], axis=1)
+    df_orario.set_axis(headers_orari,axis=1, inplace=True)
+    
+    df_orario = correzione_switch_ora_solare(df_orario)
+    
+    return (df_orario)
+
+def correzione_switch_ora_solare(df):
+    toll = 1e-06
+    headers_orari = df.columns
+    empty = [np.nan]*df.shape[0]
+    df['ora 25'] = empty
+    drop_list=[]
+    ora_switch = 3 
+    for i in range(1, df.shape[0]):
+        a = str(df[headers_orari].iloc[i-1,0])
+        b = str(df[headers_orari].iloc[i,0])
+        if a == b:  # Verifica dell'esistenza di date coincidenti su più righe del DF
+            mese = int(df[headers_orari].iloc[i-1,0][3:5]) #Estrazione dal valore 'str' il valore associato al mese 
+            
+            #ORA SOLARE devo incollare il vettore i a partire dalla colonna 3 in posizione i-1 a partire dalla colonna 2
+            if mese == 3: 
+                drop_list.append(i-1)
+                st = df.iloc[i,1:df.shape[1]].astype(float).sum() + df.iloc[i-1,1:df.shape[1]].astype(float).sum() #Verifica pre-manipolazione - somma dei valori nelle due righe    
+                for j in range (ora_switch, df.shape[1]-1): #Traslazione delle righe 
+                    df.iloc[i,j] = df.iloc[i-1,j]
+                df.iloc[i-1,24] = np.nan
+                sf = df.iloc[i,1:df.shape[1]].astype(float).sum()
+                if abs(st - sf) > toll:
+                    print(sf, st,'Mese: ', mese)
+                    sys.exit('Errore nella correzione ora solare')       
+                    
+            #ORA LEGALE incollare il vettore i a partire dalla colonna 3 ed in posizione i-1 a partire dalla colonna 4
+            elif mese == 10:
+                drop_list.append(i)
+                st = df.iloc[i,1:df.shape[1]].astype(float).sum() + df.iloc[i-1,1:df.shape[1]].astype(float).sum()
+                for j in range (ora_switch, df.shape[1]-1): #Traslazione delle righe 
+                    df.iloc[i-1,j+1] = df.iloc[i,j]
+                sf = df.iloc[i-1,1:df.shape[1]].astype(float).sum()
+                if abs(st - sf) > toll:
+                    print(sf, st, 'Mese: ', mese)
+                    sys.exit('Errore nella correzione ora solare')  
+                    
+            else:
+                print('Anomalia nei dati nel mese...')
+                print(mese)  
+                sys.exit('Verificare anomalia dati')
+                
+    if len(drop_list)>2:
+        sys.exit('Verificare anomalia dati - date coincidenti > 2')
+    for i in range (0, len(drop_list)):
+        df = df.drop([drop_list[i]])                           
+    return(df)
+
+
+def verifica_integrita_df(df_iniziale, df_finale):
+    toll = 1e-06
+    check_iniziale = df_iniziale.iloc[:,1:df_iniziale.shape[1]].astype(float).sum().sum()
+    check_finale = df_finale.iloc[:,1:df_finale.shape[1]].astype(float).sum().sum()
+    print('Energia Totale Iniziale [kWh]: ', check_iniziale, '\n','Energia Totale Finale [kWh]: ', check_finale)
+    if abs(check_iniziale - check_finale) > toll:
+        sys.exit("L'energia iniziale non coincide con quella finale")
+    print('La verifica ha avuto esito positivo')
+    return 
+
+def creazione_riepilogo_mensile(df):
+    df_mensile = df.copy()
+    df_mensile.fillna(value=0.0, inplace=True)
+    lista_mesi = []
+    for i in range(0, df_mensile.shape[0]):
+        str_mese = df_mensile.iloc[i,0]
+        lista_mesi.append(str_mese[3:5])          
+    df_mensile['Totale giornaliero [kWh]'] = df_mensile.iloc[:,1:df_mensile.shape[1]].astype(float).sum(axis=1)
+    df_mensile['mese'] = lista_mesi
+    df_mensile = df_mensile.groupby(['mese'])['Totale giornaliero [kWh]'].sum().to_frame()
+    df_mensile.reset_index(inplace=True)
+    
+    verifica_integrita_df(df, df_mensile)
+    
+    return(df_mensile)
+
+def crea_percorso (directory):
+    cwd = os.getcwd()
+    data_path = os.path.join(cwd, directory)
+    if os.path.exists(data_path) != True: 
+        os.mkdir(data_path) 
+    os.chdir(data_path)
+    return
+
+def selezione_cartella_export_dati (output_data_path, selezione_pod, selezione_anno):
+    os.chdir(output_data_path)
+    
+    crea_percorso('dati_elaborati')
+    crea_percorso(selezione_pod)
+    crea_percorso('e-dis')
+    crea_percorso(selezione_anno)
+    
+    export_path = os.getcwd()
+    return (export_path)
+
+def esporta_csv(df, nome_file, export_path, tipo_energia, tipo_regime):
+    export_path = os.path.join(export_path, nome_file + tipo_energia + '_' + tipo_regime + '.csv')
+    df.to_csv(export_path, index=False, decimal =',', sep=';')
+    return
